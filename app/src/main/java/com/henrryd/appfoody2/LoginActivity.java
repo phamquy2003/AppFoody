b3e6f40c2b85df94149cc854fd3223a9f1b89e09
phamquy2003 <anhquyk797@gmail.com>
2024-05-14 00:27:17 +0700

    xu ly quen MK

--- app/src/main/java/com/henrryd/appfoody2/LoginActivity.java
+++ app/src/main/java/com/henrryd/appfoody2/LoginActivity.java
@@ -1,10 +1,16 @@
 package com.henrryd.appfoody2;
 
 import android.annotation.SuppressLint;
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.Gravity;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -29,6 +35,7 @@ public class LoginActivity extends AppCompatActivity {
     Button btnDangNhap;
     TextView txtDangKy, txtQuenMatKhau;
     private FirebaseAuth mAuth;
+
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +49,7 @@ public class LoginActivity extends AppCompatActivity {
             return insets;
         });
         mAuth = FirebaseAuth.getInstance();
+        mAuth.signOut();
         Intent intent = getIntent();
         edEmailDN = findViewById(R.id.edEmailDK);
         edPasswordDN = findViewById(R.id.edPasswordDK);
@@ -70,6 +78,12 @@ public class LoginActivity extends AppCompatActivity {
                 register();
             }
         });
+        txtQuenMatKhau.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openForgotPassDialog(Gravity.CENTER);
+            }
+        });
 
 
     }
@@ -78,6 +92,45 @@ public class LoginActivity extends AppCompatActivity {
         Intent i = new Intent(LoginActivity.this,RegisterActivity2.class);
         startActivity(i);
 
+    }
+    private void openForgotPassDialog(int gravity){
+        final Dialog dialog = new Dialog(this);
+                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                dialog.setContentView(R.layout.activity_forgotpassword);
+        Window window = dialog.getWindow();
+        if(window == null){
+            return;
+        }
+        window.setLayout(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.WRAP_CONTENT);
+        window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        WindowManager.LayoutParams windowAttributes = window.getAttributes();
+        windowAttributes.gravity = gravity;
+        window.setAttributes(windowAttributes);
+
+        if (Gravity.BOTTOM == gravity){
+            dialog.setCancelable(true);
+        }else {
+            dialog.setCancelable(false);
+        }
+
+        EditText edEmail = dialog.findViewById(R.id.edEmail);
+        Button btnExit = dialog.findViewById(R.id.btnExit);
+        Button btnSend = dialog.findViewById(R.id.btnSend);
+
+        btnExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        dialog.show();
     }
 
     private void login() {
